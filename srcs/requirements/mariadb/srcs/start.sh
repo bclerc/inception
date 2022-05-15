8a4a74611104599d1f750a315240d12f25a8ad4d
BuildTools <unconfigured@null.spigotmc.org>
2022-05-15 22:27:06 +0200

    Fix wordpress entrypoint

--- srcs/requirements/mariadb/srcs/start.sh
+++ srcs/requirements/mariadb/srcs/start.sh
@@ -3,7 +3,7 @@ if [ ! -d /var/lib/mysql/$WP_DATABASE_NAME/ ]; then
 	mysql_install_db --datadir=/var/lib/mysql
 	/etc/init.d/mysql start
 	mysql -u root -e "CREATE DATABASE $WP_DATABASE_NAME;"
-	mysql -u root -e "GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO $WP_DATABASE_USER'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO $WP_DATABASE_USER@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
 	mysql -u root -e "FLUSH PRIVILEGES;"
 	/etc/init.d/mysql stop
 fi
--- srcs/requirements/wordpress/srcs/wordpress-entry-point.sh
+++ srcs/requirements/wordpress/srcs/wordpress-entry-point.sh
@@ -1,5 +1,5 @@
 mkdir -p /run/php/
-if [ -z "$(ls /var/www/html/)" ]; then
+if [ -z "$(ls /var/www/html/wp-content/)" ]; then
 	echo "\033[0;31m Wordpress not found, running installation ...\033[0m"
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
@@ -8,13 +8,14 @@ if [ -z "$(ls /var/www/html/)" ]; then
 	chown -R www-data:www-data		/var/www/*
 	chmod -R 755					/var/www/*
 	cd /var/www/html
-	mv /wp-config.php .
+	cp /wp-config.php .
 	wp core download --allow-root --locale=fr_FR
 	echo "Configuring wordpress ..."
 	wp core --allow-root install --url=$HOST --title="ft_inception" --admin_user=$WP_USER_NAME --admin_password=$WP_USER_PASS --admin_email=$WP_USER_MAIL || (rm -rf /var/www/* && exit 1) 
 	echo "Installing Redis wordpress plugin ..."
 	wp plugin install --url redis-cache --activate --allow-root
 	wp redis enable --force --allow-root
+else
+	echo "Starting PHP service ... "
+	php-fpm7.3 -F
 fi
-echo "Starting PHP service ... "
-php-fpm7.3 -F
