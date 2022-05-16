bbe9f3c3b017c1d2892d952c8533cec72996539d
BuildTools <unconfigured@null.spigotmc.org>
2022-05-16 05:25:14 +0200

    Ftp and group permission

--- srcs/requirements/bonus/ftp/srcs/ftp-entrypoint.sh
+++ srcs/requirements/bonus/ftp/srcs/ftp-entrypoint.sh
@@ -1,12 +1,15 @@
 if ! id -u "$FTP_USER_NAME" >/dev/null 2>&1; then
 	echo "VSFTP have no configuration, running ..."
 	useradd $FTP_USER_NAME -d /home/$FTP_USER_NAME
-	echo "$FTP_USER_NAME:$FTP_USER_PASSWORD" | chpasswd 
+	echo "$FTP_USER_NAME:$FTP_USER_PASS" | chpasswd 
 	echo "$FTP_USER_NAME" >> /etc/vsftpd.user_list
 	mkdir -p /home/$FTP_USER_NAME
 	mkdir -p /var/run/vsftpd/empty
-	chown -R $FTP_USER_NAME: /home/$FTP_USER_NAME/
+	usermod -aG wordpress $FTP_USER_NAME
+	chown -R $FTP_USER_NAME:wordpress /home/$FTP_USER_NAME/
 	chmod -R 755 /home/$FTP_USER_NAME/
+	newgrp wordpress
+	unset $FTP_USER_NAME $FTP_USER_PASS
 fi
 echo "Starting vsftpd service ... "
 vsftpd
--- srcs/requirements/wordpress/srcs/wordpress-entry-point.sh
+++ srcs/requirements/wordpress/srcs/wordpress-entry-point.sh
@@ -2,7 +2,7 @@
 if [ -z "$(ls /var/www/html/)" ]; then
 	while ! ping -c1 db
         do 
-			echo "\033[0;31mError:\033[0m Can't reachable Mysql server for installation, retrying ..." ;
+			echo "\033[0;31mError:\033[0m Can't reachable Mysql server for installation, retrying in 5s." ;
 			sleep 5;
 		done
 	echo "\033[0;31m Wordpress not found, running installation ...\033[0m"
@@ -10,15 +10,15 @@ if [ -z "$(ls /var/www/html/)" ]; then
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
 	mkdir -p /var/www/html/
-	chown -R www-data:www-data		/var/www/*
-	chmod -R 755					/var/www/*
 	cd /var/www/html
 	cp /wp-config.php .
 	wp core download --allow-root --locale=fr_FR
+	chown -R www-data:	/var/www/*
+	chmod -R 755					/var/www/*
 	echo "Configuring wordpress ..."
-	while ! wp core --allow-root install --url=$HOST --title="ft_inception" --admin_user=$WP_USER_NAME --admin_password=$WP_USER_PASS --admin_email=$WP_USER_MAIL ; then
+	while ! wp core --allow-root install --url=$HOST --title="ft_inception" --admin_user=$WP_USER_NAME --admin_password=$WP_USER_PASS --admin_email=$WP_USER_MAIL
 	do 
-		echo "\033[0;31mError:\033[0m Can't install wordpress server, retrying ..." ;
+		echo "\033[0;31mError:\033[0m Can't install wordpress server, retrying in 5s" ;
 		sleep 5;
 	done
 	echo "Installing Redis wordpress plugin ..."
