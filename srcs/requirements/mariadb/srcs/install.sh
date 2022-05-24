b79daa8b9ddad5eb68703e556ba1320b0a1de560
BuildTools <unconfigured@null.spigotmc.org>
2022-05-24 05:25:51 +0200

    Fix ftp service

--- srcs/requirements/mariadb/srcs/install.sh
+++ srcs/requirements/mariadb/srcs/install.sh
@@ -1,14 +1,14 @@
 mkfifo /var/run/mysqld/mysqld.sock
 echo "No database $WP_DATABASE_NAME found. Running ..."
-mysql_install_db --user=mysql
+mysql_install_db --user=mysql > log.tmp && rm log.tmp
 echo "Secure installation ..."
 mysqld_safe &
 mysqladmin --silent --wait=10 ping
 echo "Adding user $WP_DATABASE_USER"
 mysql -u root -e "CREATE DATABASE $WP_DATABASE_NAME;"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO $WP_DATABASE_USER@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO $WP_DATABASE_USER@'%' IDENTIFIED BY '$MARIADB_USER_PASS';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysqladmin -u root password $MARIADB_ROOT_PASSWORD
+mysqladmin -u root password $MARIADB_ROOT_PASS
 unset $(env | grep PASS | cut -d = -f 1)
 echo "Restarting"
 exit 0
